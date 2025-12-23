fee6de203743b94aedb757e3fa06345c17c97dd3
Nargiz Nusalova <nargiz.nusalova@socardownstream.az>
2025-12-23 16:40:08 +0400

    adding validation

--- models/tourModel.js
+++ models/tourModel.js
@@ -21,10 +21,16 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, 'A tour must have a difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either: easy, medium, difficult',
+      },
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0'],
     },
     ratingsQuantity: {
       type: Number,
@@ -33,7 +39,12 @@ const tourSchema = new mongoose.Schema(
     price: {
       type: Number,
     },
-    priceDiscount: Number,
+    priceDiscount: {
+      type: Number,
+      validate: function (val) {
+        return val < this.price;
+      },
+    },
     summary: {
       type: String,
       trim: true,
