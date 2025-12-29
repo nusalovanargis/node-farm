fadc8b3769dc3148680858779ed772e87b1f9fb4
Nargiz Nusalova <nargiz.nusalova@socardownstream.az>
2025-12-29 11:26:07 +0400

    adding hash password

---
+++ controllers/authController.js
@@ -0,0 +1,41 @@
+const User = require('../models/userModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.signUp = catchAsync(async (req, res, next) => {
+  const newUser = await User.create(req.body);
+
+  res.status(201).json({
+    status: 'success',
+    data: {
+      user: newUser,
+    },
+  });
+});
+
+// exports.login = async (req, res, next) => {
+//   const { email, password } = req.body;
+
+//   // 1) Check if email and password exist
+//   if (!email || !password) {
+//     return res.status(400).json({
+//       status: 'fail',
+//       message: 'Please provide email and password!',
+//     });
+//   }
+
+//   // 2) Check if user exists && password is correct
+//   const user = await User.findOne({ email }).select('+password');
+
+//   if (!user || user.password !== password) {
+//     return res.status(401).json({
+//       status: 'fail',
+//       message: 'Incorrect email or password',
+//     });
+//   }
+
+//   // 3) If everything ok, send token to client
+//   res.status(200).json({
+//     status: 'success',
+//     message: 'Logged in successfully',
+//   });
+// };
--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 // name, email, photo, password, passwordConfirm
 const userSchema = new mongoose.Schema({
@@ -10,18 +12,39 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'A user must have an email'],
     unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
   },
-  photo: {
-    type: String,
-  },
+  photo: String,
   password: {
     type: String,
     required: true,
+    minLength: 8,
+    select: false,
   },
   passwordConfirm: {
     type: String,
-    required: true,
+    required: [true, 'Please confirm your password'],
+    // This only works on CREATE and SAVE!!!
+    validator: {
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same!',
+    },
   },
 });
 
-module.exports = mongoose.model('User', userSchema);
+userSchema.pre('save', async function (next) {
+  // Only run this function if password was actually modified
+  if (!this.isModified('password')) return next();
+  // Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+  // Delete passwordConfirm field
+  this.passwordConfirm = undefined;
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
